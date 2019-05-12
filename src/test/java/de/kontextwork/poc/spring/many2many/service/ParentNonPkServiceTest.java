512de741e443dac2ee7f39be2e29a83d8f10b64b
em <eugen.mayer@kontextwork.de>
2019-05-12 13:26:55 +0200

    add the negative test and clean up a little

--- src/test/java/de/kontextwork/poc/spring/many2many/service/ParentNonPkServiceTest.java
+++ src/test/java/de/kontextwork/poc/spring/many2many/service/ParentNonPkServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.google.common.collect.Sets;
 import de.kontextwork.poc.spring.many2many.domain.nonpkservice.ChildNonPkServiceBased;
 import de.kontextwork.poc.spring.many2many.domain.nonpkservice.ParentNonPkServiceBased;
+import de.kontextwork.poc.spring.many2many.repository.ParentNonPkServiceBasedRepository;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,8 @@ class ParentNonPkServiceTest {
   @Autowired
   ParentNonPkService parentNonPkService;
 
+  @Autowired
+  ParentNonPkServiceBasedRepository parentNonPkServiceBasedRepository;
   @Autowired
   JdbcTemplate jdbcTemplate;
 
@@ -43,8 +46,29 @@ class ParentNonPkServiceTest {
     // ensure that is working
     entityManager.refresh(parent1);
 
+    // since jpa cannot load our non-pk based m2m relation, this will not be populated
+    assertEquals(
+        0,
+        parentNonPkServiceBasedRepository.findById(parent1.getParentId()).orElseThrow().getChildren().size()
+    );
+
     // using the service, it should have been populated now
     var reloaded = parentNonPkService.getParent(parent1.getParentId());
     assertEquals(2, reloaded.orElseThrow().getChildren().size());
+  }
+
+
+  @Test
+  void saveParent() {
+    // one child presaved
+    var child1 = new ChildNonPkServiceBased("child1");
+    // one not presaved
+    var child2notSaved = new ChildNonPkServiceBased("child2");
+
+    var parent1 = new ParentNonPkServiceBased();
+    parent1.setChildren(
+        Sets.newHashSet(child1, child2notSaved)
+    );
+    parentNonPkService.save(parent1);
   }
 }
