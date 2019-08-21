30c8e91db482fbbbd4e2daa39a493d1315f049bc
em <eugen.mayer@kontextwork.de>
2019-08-21 11:06:00 +0200

    remove the adhoc association test, since it basically falls into the same case as the natural association

--- src/main/java/de/kontextwork/poc/spring/many2many/ahhocassociation/company/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package de.kontextwork.poc.spring.many2many.ahhocassociation.company;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompanyRepository extends JpaRepository<Company, Long>
-{
-}
--- src/main/java/de/kontextwork/poc/spring/many2many/ahhocassociation/role/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package de.kontextwork.poc.spring.many2many.ahhocassociation.role;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long>
-{
-}
