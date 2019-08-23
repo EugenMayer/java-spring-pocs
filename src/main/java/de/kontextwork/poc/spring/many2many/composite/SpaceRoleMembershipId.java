53e567a9c65b4b5f34170370a9f6f97876562b1b
em <eugen.mayer@kontextwork.de>
2019-08-23 11:32:59 +0200

    move column defintions out of the EmbeddedId since they are never used, they have to be defined in the actual entity as JoinColumn

--- src/main/java/de/kontextwork/poc/spring/many2many/composite/SpaceRoleMembershipId.java
+++ src/main/java/de/kontextwork/poc/spring/many2many/composite/SpaceRoleMembershipId.java
@@ -12,14 +12,11 @@ import lombok.EqualsAndHashCode;
 class SpaceRoleMembershipId implements Serializable
 {
   @EqualsAndHashCode.Include
-  @Column(name = "space_name", length = 10)
   private String spaceName;
 
   @EqualsAndHashCode.Include
-  @Column(name = "user_username", length = 10)
   private String userUsername;
 
   @EqualsAndHashCode.Include
-  @Column(name = "space_role_name", length = 10)
   private String spaceRoleName;
 }
