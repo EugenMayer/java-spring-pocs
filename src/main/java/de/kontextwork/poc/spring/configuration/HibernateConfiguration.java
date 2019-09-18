00eb25b959315f89c40eeea811d83f3e5f3342ef
em <eugen.mayer@kontextwork.de>
2019-09-18 09:21:18 +0200

    make bp configuration optional

--- src/main/java/de/kontextwork/poc/spring/configuration/HibernateConfiguration.java
+++ src/main/java/de/kontextwork/poc/spring/configuration/HibernateConfiguration.java
@@ -1,15 +1,17 @@
 package de.kontextwork.poc.spring.configuration;
 
-import com.blazebit.persistence.spring.data.impl.repository.BlazePersistenceRepositoryFactoryBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaRepositories(
-  basePackages = "de.kontextwork.poc.spring",
-  // @see https://persistence.blazebit.com/documentation/1.4/entity-view/manual/en_US/#spring-data-setup
-  repositoryFactoryBeanClass = BlazePersistenceRepositoryFactoryBean.class
+  basePackages = "de.kontextwork.poc.spring"
 )
+@ConditionalOnProperty(
+  value = "blazepersistance.enabled",
+  havingValue = "false",
+  matchIfMissing = true)
 public class HibernateConfiguration
 {
 }
