a4b8220bf47cb4647e03ec64f43c75a353722b34
em <eugen.mayer@kontextwork.de>
2019-11-02 18:20:39 +0100

    polish the creatable embeddedIdView

--- src/main/java/de/kontextwork/poc/spring/blaze/fullapp/rolemembership/model/view/RealmRoleMembershipEmbeddedIdCreateView.java
+++ src/main/java/de/kontextwork/poc/spring/blaze/fullapp/rolemembership/model/view/RealmRoleMembershipEmbeddedIdCreateView.java
@@ -1,9 +1,11 @@
 package de.kontextwork.poc.spring.blaze.fullapp.rolemembership.model.view;
 
-import com.blazebit.persistence.view.EntityView;
+import com.blazebit.persistence.view.*;
 import de.kontextwork.poc.spring.blaze.fullapp.rolemembership.model.jpa.RealmRoleMembershipId;
 
 @EntityView(RealmRoleMembershipId.class)
+@CreatableEntityView
+@UpdatableEntityView
 public interface RealmRoleMembershipEmbeddedIdCreateView extends RealmRoleMembershipEmbeddedIdView
 {
   void setSubjectId(Long subjectId);
