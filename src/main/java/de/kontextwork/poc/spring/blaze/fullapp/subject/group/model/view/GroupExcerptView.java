d04c7d1bdbb790224467ed2627dbacbef863a5f4
em <eugen.mayer@kontextwork.de>
2019-11-05 11:44:30 +0100

    reproduce bug with mapping

--- src/main/java/de/kontextwork/poc/spring/blaze/fullapp/subject/group/model/view/GroupExcerptView.java
+++ src/main/java/de/kontextwork/poc/spring/blaze/fullapp/subject/group/model/view/GroupExcerptView.java
@@ -1,6 +1,7 @@
 package de.kontextwork.poc.spring.blaze.fullapp.subject.group.model.view;
 
 import com.blazebit.persistence.view.EntityView;
+import com.blazebit.persistence.view.Mapping;
 import de.kontextwork.poc.spring.blaze.fullapp.subject.group.model.jpa.Group;
 import de.kontextwork.poc.spring.blaze.fullapp.subject.user.model.view.UserExcerptView;
 import de.kontextwork.poc.spring.blaze.fullapp.subject.model.view.SubjectExcerptView;
@@ -12,4 +13,7 @@ public interface GroupExcerptView extends SubjectExcerptView, GroupIdView
   String getName();
 
   Set<UserExcerptView> getMembers();
+
+  @Mapping("SIZE(members)")
+  Long getMemberCount();
 }
