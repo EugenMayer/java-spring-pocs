e36022646ca54f10152bbf0807cd95ec387fd981
em <eugen.mayer@kontextwork.de>
2019-11-08 11:28:48 +0100

    fix extra members

--- src/main/java/de/kontextwork/poc/spring/blaze/fullapp/subject/group/model/view/GroupExcerptView.java
+++ src/main/java/de/kontextwork/poc/spring/blaze/fullapp/subject/group/model/view/GroupExcerptView.java
@@ -15,8 +15,6 @@ public interface GroupExcerptView extends SubjectExcerptView, GroupIdView
   @Mapping("groupMachine")
   String getMachine();
 
-  Set<UserExcerptView> getMembers();
-
   // deactivated dut to https://github.com/Blazebit/blaze-persistence/issues/905 .. until Alpha4 is released
   @Mapping("SIZE(members)")
   Long getMemberCount();
