31784f2de33ac1c483c50011e2d311e528f4444e
em <eugen.mayer@kontextwork.de>
2019-11-11 08:27:38 +0100

    use BP snapshot

--- src/main/java/de/kontextwork/poc/spring/blaze/fullapp/subject/group/model/view/GroupMemberUpdateView.java
+++ src/main/java/de/kontextwork/poc/spring/blaze/fullapp/subject/group/model/view/GroupMemberUpdateView.java
@@ -2,7 +2,6 @@ package de.kontextwork.poc.spring.blaze.fullapp.subject.group.model.view;
 
 import com.blazebit.persistence.view.*;
 import de.kontextwork.poc.spring.blaze.fullapp.subject.group.model.jpa.Group;
-import de.kontextwork.poc.spring.blaze.fullapp.subject.user.model.jpa.User;
 import de.kontextwork.poc.spring.blaze.fullapp.subject.user.model.view.UserIdView;
 import java.util.Set;
 
